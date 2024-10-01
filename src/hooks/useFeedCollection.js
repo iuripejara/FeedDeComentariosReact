f23270bba898eba4e110ccc457999105e5f586c4
iuripejara <iuripp91@gmail.com>
2024-10-01 16:24:57 +0100

    Update useFeedCollection.js

--- src/hooks/useFeedCollection.js
+++ src/hooks/useFeedCollection.js
@@ -1,16 +1,17 @@
 import { useState } from "react"
 
 export default function useFeedCollection(){
+
     const [feed,setFeed] = useState(()=>{
         const storedFeed = localStorage.getItem("abc-feed-lib")
         if(!storedFeed)return[]
         return JSON.parse(storedFeed)
     })
+
     const addFeed =({email,comentario})=>{
         const id = Math.floor(Math.random()*1000)
 
         const feed = {id,email,comentario}
-        
         setFeed(State =>{
             const newState = [...State,feed] 
             localStorage.setItem("abc-feed-lib",JSON.stringify(newState))
@@ -18,6 +19,7 @@ export default function useFeedCollection(){
         })
         console.log(hors)
     }
+
     const removeFeed = (id) => {
         setFeed(State =>{
           const newState = State.filter(feed => feed.id !== id)
